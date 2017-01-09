dc2abb03c1de0bf0d1cd4e7fe16e1f352e071615
Victor <victor.yap@utexas.edu>
2017-01-09 09:54:14 -0600

    rm oh-my-zsh, add to ubuntu_bootstrap

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -6,11 +6,12 @@ sudo -v
 
 DEBIAN_FRONTEND=noninteractive 
 
-PROGRAMS=(stow xorg i3 rxvt-unicode vim)
-PACKAGES=(i3 i3status Xresources uxrvt vim)
+PROGRAMS=(stow xorg i3 rxvt-unicode vim tmux zsh)
+PACKAGES=(i3 i3status Xresources uxrvt vim tmux zsh)
 
 sudo apt-get update
 sudo apt-get -y install ${PROGRAMS[*]}
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 stow ${PACKAGES[*]}
 
 xrdb $HOME/.Xresources
