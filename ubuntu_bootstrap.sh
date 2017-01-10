f9510141fbb2929d69f62005a98cdda7bf6e5aaa
Victor Yap <vicyap@years-of-war.att.net>
2017-01-10 03:41:46 -0600

    added wallpaper

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -7,12 +7,13 @@ sudo -v
 DEBIAN_FRONTEND=noninteractive 
 
 PROGRAMS=(stow xorg i3 rxvt-unicode vim tmux zsh python-pip)
-PACKAGES=(i3 i3status Xresources uxrvt vim tmux virtualenvs zsh)
+PACKAGES=(i3 i3status Xresources uxrvt vim tmux virtualenvs wallpaper zsh)
 PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper)
 
 sudo apt-get update
 sudo apt-get -y install ${PROGRAMS[*]}
 pip install ${PIP_PACKAGES[*]} --user
+sudo pip install git+https://github.com/ultrabug/py3status
 stow ${PACKAGES[*]}
 
 xrdb $HOME/.Xresources
