7909999765a0984360e0799809be2261c581e242
Victor Yap <vicyap@years-of-war.att.net>
2017-01-10 02:35:02 -0600

    update install script, add gitignore

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -6,16 +6,15 @@ sudo -v
 
 DEBIAN_FRONTEND=noninteractive 
 
-PROGRAMS=(stow xorg i3 rxvt-unicode vim tmux zsh pip)
+PROGRAMS=(stow xorg i3 rxvt-unicode vim tmux zsh python-pip)
 PACKAGES=(i3 i3status Xresources uxrvt vim tmux virtualenvs zsh)
 PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper)
 
 sudo apt-get update
 sudo apt-get -y install ${PROGRAMS[*]}
 pip install ${PIP_PACKAGES[*]} --user
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv ~/.zshrc ~/.zshrc.template
 stow ${PACKAGES[*]}
 
 xrdb $HOME/.Xresources
 echo "You may have to restart apps or reload configs to take effect"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
