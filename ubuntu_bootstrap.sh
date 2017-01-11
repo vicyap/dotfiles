b4aebd7a570b60924df63fe621145d8bcb6c6d5f
Victor Yap <victor.yap@utexas.edu>
2017-01-11 08:39:17 +0000

    update pip installs

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -4,14 +4,13 @@ set -e
 
 DEBIAN_FRONTEND=noninteractive 
 
-PROGRAMS=(stow xorg i3 rxvt-unicode vim tmux zsh python-pip)
+PROGRAMS=(stow xorg i3 rxvt-unicode vim task tmux zsh python-pip)
 PACKAGES=(i3 i3status Xresources urxvt vim tmux virtualenvs wallpaper zsh)
-PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper)
+PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper git+https://github.com/ultrabug/py3status)
 
 sudo apt-get update
 sudo apt-get -y install ${PROGRAMS[*]}
-pip install ${PIP_PACKAGES[*]} --user
-sudo pip install git+https://github.com/ultrabug/py3status
+sudo pip install ${PIP_PACKAGES[*]}
 stow ${PACKAGES[*]}
 
 xrdb $HOME/.Xresources
