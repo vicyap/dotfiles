87d05a4e55c26586a80659d651e295edea10ba98
Victor Yap <victor.yap@utexas.edu>
2017-01-11 11:11:08 -0600

    update ubuntu_bootstrap.sh

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -5,13 +5,14 @@ set -e
 DEBIAN_FRONTEND=noninteractive 
 
 PROGRAMS=(stow xorg i3 rxvt-unicode vim task tmux zsh python-pip)
-PACKAGES=(i3 i3status Xresources urxvt vim tmux virtualenvs wallpaper zsh)
 PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper git+https://github.com/ultrabug/py3status)
 
 sudo apt-get update
 sudo apt-get -y install ${PROGRAMS[*]}
 sudo pip install ${PIP_PACKAGES[*]}
-stow ${PACKAGES[*]}
+rm ~/.virtualenvs/postactivate
+rm ~/.virtualenvs/postdeactivate
+./stow_packages.sh
 
-xrdb $HOME/.Xresources
 ./install_oh_my_zsh.sh
+xrdb $HOME/.Xresources
