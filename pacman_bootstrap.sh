cc63f47fe0797b0aa63cf00a94494d06d04b95dc
Victor Yap <victor.yap@utexas.edu>
2017-02-17 08:25:47 +0000

    adding pacman bootstrap

---
+++ pacman_bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+PROGRAMS=(stow xorg i3 rxvt-unicode vim task tmux zsh python-pip)
+PIP_PACKAGES=(autoenv virtualenv virtualenvwrapper git+https://github.com/ultrabug/py3status)
+
+sudo pacman -Syu --noconfirm
+sudo pacman -Sy --noconfirm ${PROGRAMS[*]}
+sudo pip install ${PIP_PACKAGES[*]}
+rm -f ~/.virtualenvs/postactivate
+rm -f ~/.virtualenvs/postdeactivate
+./stow_packages.sh
+
+git clone git://github.com/yonchu/zsh-vcs-prompt.git ~/.zsh/custom/zsh-vcs-prompt
+
+./install_oh_my_zsh.sh
+xrdb $HOME/.Xresources
