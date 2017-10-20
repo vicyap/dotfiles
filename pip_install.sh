2ce3d648fe1cf692b62355c2296e2f9d9df6d262
Victor Yap <victor@vicyap.com>
2017-10-20 06:49:07 +0000

    remove olds scripts that don't really work

--- pip_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# SUDO_PIP_PACKAGES=(git+https://github.com/ultrabug/py3status)
-USER_PIP_PACKAGES=(virtualenv virtualenvwrapper)
-
-# sudo pip install ${SUDO_PIP_PACKAGES[*]}
-pip install ${USER_PIP_PACKAGES[*]} --user
