5ea134b63d43ab134ff74ac5256fac8a87860f8c
Victor Yap <victor@vicyap.com>
2017-10-20 07:23:18 +0000

    move scripts to top leve

---
+++ ansible_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+source ./variables.sh
+
+ansible-playbook \
+    --syntax-check \
+    --inventory-file "$ANSIBLE_INVENTORY_FILE" \
+    "$ANSIBLE_PLAYBOOK_FILE"
+
+# shellcheck disable=SC2086
+shellcheck $SHELLCHECK_FILES
--- scripts/variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# shellcheck disable=SC2034
-
-ANSIBLE_INVENTORY_FILE='../ansible/hosts'
-ANSIBLE_PLAYBOOK_FILE='../ansible/dev.yml'
-ANSIBLE_CONNECTION_TYPE='local'
-ANSIBLE_NUM_FORKS='1'
