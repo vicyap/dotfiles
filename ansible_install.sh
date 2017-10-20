41986cb9619b066b435d60c74113f75a2753da25
Victor Yap <victor@vicyap.com>
2017-10-20 07:29:41 +0000

    I'm running out of excuses

--- ansible_install.sh
+++ ansible_install.sh
@@ -6,7 +6,7 @@ set -e
 source ./variables.sh
 
 ansible-playbook \
-    --forks "ANSIBLE_NUM_FORKS" \
-    --connection "ANSIBLE_CONNECTION_TYPE" \
-    --inventory-file "ANSIBLE_INVENTORY_FILE" \
-    "ANSIBLE_PLAYBOOK_FILE"
+    --forks "$ANSIBLE_NUM_FORKS" \
+    --connection "$ANSIBLE_CONNECTION_TYPE" \
+    --inventory-file "$ANSIBLE_INVENTORY_FILE" \
+    "$ANSIBLE_PLAYBOOK_FILE"
